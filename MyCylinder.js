99ec63dfd4b5ed2dfd5001111017dbb474f29053
J. Luz <up201703782@fe.up.pt>
2019-10-04 00:18:37 +0100

    Added first test scene and MySphere class
    Co-authored-by: lilianalmeida <up201706908@fe.up.pt>
    Co-authored-by: joaohenriqueluz <up201703782@fe.up.pt>

---
+++ MyCylinder.js
@@ -0,0 +1,79 @@
+/**
+ * MyCylinder
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+class MyCylinder extends CGFobject {
+	constructor(scene, id, base, top, height, slices, stacks) {
+		super(scene);
+		this.base = base;
+		this.top = top;
+		this.height = height;
+		this.slices = slices;
+		this.stacks = stacks;
+
+		this.initBuffers();
+	}
+
+	initBuffers() {
+
+		this.vertices = [];
+		this.indices = [];
+		this.normals = [];
+		this.texCoords = [];
+
+		var subHeight = 0;
+		var diffHeight = this.height / this.stacks;
+
+		var radius = this.base;
+		var diffRadius = (this.top - this.base) / this.stacks;
+
+		for (var j = 0; j <= this.stacks; j++) {
+
+			var ang = 0;
+			var alphaAng = 2 * Math.PI / this.slices;
+			for (var i = 0; i <= this.slices; i++) {
+
+				var sa = Math.sin(ang);
+				var saa = Math.sin(ang + alphaAng);
+				var ca = Math.cos(ang);
+				var caa = Math.cos(ang + alphaAng);
+
+				this.vertices.push(sa * radius, ca * radius, subHeight); 
+
+				//TO DO!
+				this.texCoords.push(ang / (Math.PI * 2), subHeight);
+
+				//TO DO!!
+				this.normals.push(
+					sa,
+					ca,
+					0
+				);		
+
+				ang += alphaAng;
+			}
+
+			subHeight += diffHeight;
+			radius += diffRadius;
+		}
+
+		for (var j = 0; j < this.stacks; j++) {
+			for (var i = j * (this.slices + 1); i < j * (this.slices + 1) + this.slices; i++) {
+				this.indices.push((i + this.slices + 1), (i + 1), (i));
+				this.indices.push((i + 1), (i + this.slices + 1), (i + this.slices + 2));
+			}
+		}
+
+		this.primitiveType = this.scene.gl.TRIANGLES;
+		this.initGLBuffers();
+	}
+
+	updateBuffers(complexity) {
+		//this.slices = 3 + Math.round(9 * complexity); //complexity varies 0-1, so slices varies 3-12
+
+		// reinitialize buffers
+		this.initBuffers();
+		this.initNormalVizBuffers();
+	}
+}
---
+++ MyTriangle.js
@@ -0,0 +1,50 @@
+/**
+ * MyTriangle
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+class MyTriangle extends CGFobject {
+	constructor(scene, id, x1, x2, x3, y1, y2, y3, z1, z2, z3) {
+		super(scene);
+		this.x1 = x1;
+		this.x2 = x2;
+		this.x3 = x3;
+		this.y1 = y1;
+		this.y2 = y2;
+		this.y3 = y3;
+		this.z1 = z1;
+		this.z2 = z2;
+		this.z3 = z3;
+
+		this.initBuffers();
+	}
+	initBuffers() {
+		this.vertices = [
+			this.x1, this.y1, this.z1, 	//0
+			this.x2, this.y2, this.z2,	//1
+			this.x3, this.y3, this.z3	//2
+		];
+
+		//Counter-clockwise reference of vertices
+		this.indices = [
+			0,1,2	
+		];
+
+		this.normals= [
+			0,0,1,
+			0,0,1,
+			0,0,1
+		];
+
+		this.texCoords = [
+			0,1,
+			0.5,1,
+			0,0.5
+		];
+		
+		this.primitiveType = this.scene.gl.TRIANGLES;
+		this.initGLBuffers();
+	}
+	
+}
+
